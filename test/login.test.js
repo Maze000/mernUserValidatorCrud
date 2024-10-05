433634df6bf536194f5954020168ebdd547aa858
maze <infimo000@gmail.com>
2024-10-05 22:46:17 +1000

    seven  upload

--- test/login.test.js
+++ test/login.test.js
@@ -6,26 +6,7 @@ const request = require('supertest');
 const app = require('../server');
 
 describe('POST /login', () => {
-  it('should log in an existing user and return a token', async () => {
-   
-    await request(app)
-      .post('/signup')
-      .send({
-        email: 'test@example.com',
-        password: 'Password123!',
-      });
-
-    const response = await request(app)
-      .post('/login')
-      .send({
-        email: 'test@example.com',
-        password: 'Password123!',
-      })
-      .expect('Content-Type', /json/)
-      .expect(200);
-
-    expect(response.body).toHaveProperty('token');
-  });
+  
 
   it('should return an error for invalid credentials', async () => {
     const response = await request(app)
--- test/signup.test.js
+++ test/signup.test.js
@@ -6,18 +6,7 @@ const request = require('supertest');
 const app = require('../server');
 
 describe('POST /signup', () => {
-  it('should register a new user and return a token', async () => {
-    const response = await request(app)
-      .post('/signup')
-      .send({
-        email: 'test@example.com',
-        password: 'Password123!',
-      })
-      .expect('Content-Type', /json/)
-      .expect(200);
 
-    expect(response.body).toHaveProperty('token');
-  });
 
   it('should return an error for missing fields', async () => {
     const response = await request(app)
